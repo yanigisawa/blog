4a79a716f76847209812d7bdbb3f02947fac7981
James Alexander <jalexander@leafsoftwaresolutions.com>
2015-10-25 00:51:40 -0400

    - Changing theme to the { Personal } theme - Adding all posts to repository. - Altering minor nuances of the theme, specifically making the auto scroll display the header in the page body. (using a spacer div)

--- blog-import.rb
+++
@@ -1,8 +0,0 @@
-require "jekyll-import";
-JekyllImport::Importers::Drupal7.run({
-    "dbname"   => "jamesralexandercom_drupal",
-    "user"     => "yanigisawa",
-    "password" => "Vh2De49eFPNGjmKRTMs8",
-    "host"     => "db.jamesralexander.com",
-    "prefix"   => "wl_"
-})
